66af3390880d2c7cc8f851adaae8d4c3d2175aa7
RHA Nijhuis <r.h.a.nijhuis@student.tue.nl>
2018-03-08 15:26:13 +0100

    Add methods to add and get nodes to/from a Building

--- src/nl/tue/robots/drones/model/Building.java
+++ src/nl/tue/robots/drones/model/Building.java
@@ -15,7 +15,28 @@ public class Building {
         return new ArrayList<>(nodes.values());
     }
 
+    /**
+     * Retrieves the Node with the specified ID.
+     * @param nodeID The node which to retrieve
+     * @return The found node or {@code null} if none exists
+     */
+    public Node getNode(int nodeID) {
+        return nodes.get(nodeID);
+    }
 
+    /**
+     * Adds the new node with the specified ID to the building if no node with this ID exists yet.
+     * @param n The ode to add
+     * @param nodeID The ID of the new node
+     * @return
+     */
+    public void addNode(Node n, int nodeID) {
+        if (nodes.get(nodeID) != null) {
+            throw new IllegalArgumentException("A node with this ID already exists");
+        }
+
+        nodes.put(nodeID, n);
+    }
 
 
 }
